71107416f6b0f23bd2b705d47545f0e4faf24993
ghghyo <67969887+ghghyo@users.noreply.github.com>
2022-02-19 19:22:37 -0500

    Add files via upload

---
+++ golem.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -sSf https://join.golem.network/as-requestor | bash -
+yagna service run
+yagna app-key create requestor
+yagna app-key list
+yagna payment fund
+yagna payment status
+yagna payment init --sender
+
