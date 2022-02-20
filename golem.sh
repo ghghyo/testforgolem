b0044f5aa6a1e892702fffc81625118326fef87f
ghghyo <67969887+ghghyo@users.noreply.github.com>
2022-02-19 19:26:08 -0500

    Delete golem.sh

--- golem.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-curl -sSf https://join.golem.network/as-requestor | bash -
-yagna service run
-yagna app-key create requestor
-yagna app-key list
-yagna payment fund
-yagna payment status
-yagna payment init --sender
-
